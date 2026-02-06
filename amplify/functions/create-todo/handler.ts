635f7be405a57e4f97a690d1b4350fa1d9effdcc
suman7802 <mansu7802@gmail.com>
2026-02-06 19:20:36 +0545

    feat: added testing todo

--- amplify/functions/create-todo/handler.ts
+++ amplify/functions/create-todo/handler.ts
@@ -7,5 +7,15 @@ import { createResponse } from '../../shared/utils/response.util';
 export const handler: Handler = apiHandler(async (event, context) => {
   logger.crud.info('create todo fn event', { event });
 
-  return createResponse(201, { message: 'Todo created successfully' }, event);
+  const result = await dbClient({
+    action: 'create',
+    table: 'Todo',
+    item: {
+      userId: '123',
+      title: 'testing',
+      content: 'testing',
+    },
+  });
+
+  return createResponse(201, { message: 'Todo created successfully', result }, event);
 });
